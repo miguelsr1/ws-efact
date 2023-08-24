72b609cc7c4237acff373aca1183657f69fd0e6a
migue <miguelsr1@gmail.com>
2023-08-24 00:35:34 -0600

    Inicial

--- src/main/java/sv/com/jsoft/ws/efact/GreetingResource.java
+++ src/main/java/sv/com/jsoft/ws/efact/GreetingResource.java
@@ -1,16 +1,14 @@
 package sv.com.jsoft.ws.efact;
 
 import io.quarkus.logging.Log;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import org.primefaces.model.rest.AutoCompleteSuggestion;
 import org.primefaces.model.rest.AutoCompleteSuggestionResponse;
 import sv.com.jsoft.ws.efact.model.Producto;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Path("/hello")
@@ -43,4 +41,15 @@ public class GreetingResource {
                 .map(t -> new AutoCompleteSuggestion(Integer.toString(t.idProducto), t.nombre))
                 .collect(Collectors.toList()));
     }
+
+
+    @GET
+    @Path("/producto/{codigo}/")
+    @Produces({MediaType.APPLICATION_JSON})
+    public Producto productoByCodigo(@PathParam("codigo") String codigo) {
+        Log.info("ok");
+        Optional<Producto> optional = Producto.findByIdOptional(codigo);
+        return optional.orElseThrow(NotFoundException::new);
+    }
+
 }
